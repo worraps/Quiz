172f9377f297c9996cea4a1e622ebd2f47d07f45
Kamil <Burzyk>
2022-02-16 22:43:04 +0100

    Initial Commit

---
+++ Quizz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizz
+//
+//  Created by Kamil Burzyk on 16/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
